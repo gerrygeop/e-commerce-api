316518d94b52551126dc234659822b8837e23e69
gerrygeop <gerryja28@gmail.com>
2022-07-10 17:32:25 +0800

    implement all router

--- index.js
+++ index.js
@@ -5,19 +5,25 @@ const dotenv = require("dotenv");
 const userRoute = require("./routes/user");
 const authRoute = require("./routes/auth");
 const productRoute = require("./routes/product");
+const cartRoute = require("./routes/cart");
+const orderRoute = require("./routes/order");
+const stripeRoute = require("./routes/stripe");
 
 dotenv.config();
 
 mongoose
    .connect(process.env.MONGO_URL)
    .then(() => console.log("DB connection successfull"))
-   .catch((err) => console.log(err) );
+   .catch((err) => console.log(err));
 
 app.use(express.json());
 
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/carts", cartRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
    console.log("Backend server is running");
